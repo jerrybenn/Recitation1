b698a77b0f134355a55ca9f255ae2f241375d98f
JerryBenn <jerrydarobenn@gmail.com>
2023-01-21 21:58:38 -0500

    Had to commit

--- src/main/java/BasicInput.java
+++ src/main/java/BasicInput.java
@@ -5,16 +5,24 @@ public class BasicInput {
         Scanner scnr = new Scanner(System.in);
         int userInt = 0;
         double userDouble = 0.0;
-        // TODO Define char and string variables similarly
+        char userChar;
+        String userString;
 
+        // TODO Define char and string variables similarly
         System.out.println("Enter integer:");
         userInt = scnr.nextInt();
-
+        System.out.println("Enter double:");
+        userDouble = scnr.nextDouble();
+        System.out.println("Enter character:");
+        userChar = scnr.next().charAt(0);
+        System.out.println("Enter string:");
+        userString = scnr.next();
         // TODO (1): Finish reading other items into variables, then output the four values on a single line separated by a space
-
+        System.out.println(userInt +" "+userDouble+" "+ userChar +" "+userString);
         // TODO (2): Output the four values in reverse
+        System.out.println(userString +" "+ userChar +" "+ userDouble +" "+ userInt);
 
         // TODO (3): Cast the double to an integer, and output that integer
-
+        System.out.println(userDouble +" "+ "cast to an integer is"+ " " + (int)userDouble);
     }
 }
